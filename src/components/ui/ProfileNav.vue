bc919be63d2c980fbb53568ce916b70470951dfd
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2023-09-06 16:19:53 +0000

    add subscribe

--- src/components/ui/ProfileNav.vue
+++ src/components/ui/ProfileNav.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="headwrapper">
         <nav style="background: #fff;" class="d-flex justify-content-between align-items-center py-4 header-nav" id="">
-            <ul style="margin-bottom:0px" class="d-flex justify-content-between p-0 align-items-center col-3">
+            <ul style="margin-bottom:0px" class="d-flex justify-content-between p-0 align-items-center col-2">
                 <li class="item button allitemmargin"><a class="btn custom-btn-blank btn-sm px-5 py-0" href="/"><pre>        </pre></a></li>
             </ul>
-            <ul style="margin-bottom:0px" class="d-flex justify-content-between align-items-center col-5">
+            <ul style="margin-bottom:0px" class="d-flex justify-content-between align-items-center col-6">
                 <li class="item button allitemmargin"><a class="btn custom-btn-bg btn-sm px-5 py-2" href="#">My Profile</a></li>
             </ul>
         </nav>
